fa2c8c8cb0dc2e2af2f35656f7eeb87906d5dbc2
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-12 22:05:18 +0600

    array of object, includes, filter, forEach, map recap

--- Module-37/array-object.js
+++ Module-37/array-object.js
@@ -1,9 +1,21 @@
 const product = [
     {brand:'lenovo', item:'laptop'},
-    {brand:'apple', item:'laptop'},
+    {brand:'appley', item:'laptop'},
     {brand:'samsung', item:'laptop'},
-    {brand:'asus', item:'laptop'},
+    {brand:'asusy', item:'laptop'},
 ]
 
-const brands = product.map(brandName = brandName.brand);
+// you can create brand new array-of-object using map from previous array-of-object to get just individual items of something.
+
+const brands = product.map(brandName => brandName.brand);
 console.log(brands)
+
+
+// Difference between map and forEach is getting output through variables name. You will array-like-value by map , but you can't create variables for forEach.
+
+product.forEach(brndName => console.log(brndName.brand))
+
+
+
+const findYLetterProducts = product.filter(y => y.brand.includes('y'))
+console.log(findYLetterProducts)
