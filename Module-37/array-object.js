eaec05669cfe5e1b2999a70a41b91d75ed194c31
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-12 21:45:27 +0600

    initial repo of milestone-7

---
+++ Module-37/array-object.js
@@ -0,0 +1,9 @@
+const product = [
+    {brand:'lenovo', item:'laptop'},
+    {brand:'apple', item:'laptop'},
+    {brand:'samsung', item:'laptop'},
+    {brand:'asus', item:'laptop'},
+]
+
+const brands = product.map(brandName = brandName.brand);
+console.log(brands)
